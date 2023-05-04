c8d01f1bc2d39a44f0993b1c5abd3e156afa6172
Boubamahir2 <amamahir1@gmail.com>
2023-05-04 02:53:49 +0200

    utilties functions

--- backend/utils/utility.js
+++ backend/utils/utility.js
@@ -1,3 +1,9 @@
+import jwt from 'jsonwebtoken';
+import models from '../models/index.js';
+import ResponseMessages from '../constants/responseMessages.js';
+
+const utility = {};
+
 utility.generateAuthToken = async (user) => {
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_LIFETIME,
@@ -14,6 +20,16 @@ utility.generateAuthToken = async (user) => {
   return authToken;
 };
 
+// Check Username Availability
+utility.checkUsernameAvailable = async (username) => {
+  let user = await models.User.findOne({ username });
+
+  if (user) {
+    return false;
+  }
+  return true;
+};
+
 utility.deleteExpiredAuthTokens = async () => {
   console.log('[cron] task to delete expired auth tokens has started.');
   const authTokens = await models.AuthToken.find({
@@ -28,3 +44,35 @@ utility.deleteExpiredAuthTokens = async () => {
     console.log('[cron] No expired auth tokens found.');
   }
 };
+
+// POST OWNER
+utility.checkIfPostOwner = async (postId, user) => {
+  const post = await models.Post.findById(postId).select('owner');
+
+  if (post.owner.toString() === user._id.toString()) {
+    return true;
+  }
+
+  return false;
+};
+
+utility.checkEmailAvailable = async (email) => {
+  let user = await models.User.findOne({ email });
+
+  if (user) {
+    return false;
+  }
+
+  return true;
+};
+
+// CHECK IF SAME USER
+utility.checkIfSameUser = async (user, userId) => {
+  if (user._id.toString() === userId.toString()) {
+    return true;
+  }
+
+  return false;
+};
+
+export default utility;
